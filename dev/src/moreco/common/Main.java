ad8751d34de7bb9da178c3c27b2204eb7f696da4
Florian Mathis <hello@florianmathis.com>
2015-10-15 12:17:00 +0200

    Added example model generator

--- dev/src/moreco/common/Main.java
+++ dev/src/moreco/common/Main.java
@@ -42,7 +42,7 @@ public class Main extends AbstractAcceleoGenerator {
      *
      * @generated
      */
-    public static final String[] TEMPLATE_NAMES = { "generateMain" };
+    public static final String[] TEMPLATE_NAMES = { "generateElement" };
     
     /**
      * The list of properties files from the launch parameters (Launch configuration).
