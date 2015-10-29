bf3473dad78f3547dcc9d1418f1621160cfd9f91
Florian Mathis <hello@florianmathis.com>
2015-10-29 12:08:08 +0100

    moved main package

--- moreco/src/moreco/main/Main.java
+++ moreco/src/moreco/main/Main.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-package moreco.common;
+package moreco.main;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,7 +35,7 @@ public class Main extends AbstractAcceleoGenerator {
      *
      * @generated
      */
-    public static final String MODULE_FILE_NAME = "/moreco/common/main";
+    public static final String MODULE_FILE_NAME = "/moreco/main/main";
     
     /**
      * The name of the templates that are to be generated.
